7e411f6ac4000d129e99745c921a014c8a110d56
tranhuyhoangbka <hoangth92.nd@gmail.com>
2016-02-15 17:27:00 +0700

    update

--- app/workers/hard_worker.rb
+++ app/workers/hard_worker.rb
@@ -1,5 +1,10 @@
 class HardWorker
   include Sidekiq::Worker
+
+  sidekiq_retries_exhausted do |msg|
+    Sidekiq.logger.warn "Failed #{msg['class']} with #{msg['args']}: #{msg['error_message']}"
+  end
+
   def perform name, count
     5.times{|n| puts "#{n}. this is #{name} \n"}
   end
